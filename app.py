7949dd5ca7f31ad7d177afd1fc13a90de2f88d2a
Eric Ngo <quocdat32461997@yahoo.com>
2020-10-28 11:54:50 -0500

    Clean app.py

--- app.py
+++ app.py
@@ -8,18 +8,23 @@ from flask import Flask, request, jsonify
 import boto3
 import logging
 
-from lib import *
+from lib import initialize
 
 # initialize Flask app
 app = Flask(__name__)
 
+def main():
+	connection = initialize()
+	app.run()
+
 @app.route('/')
 def hi():
 	return 'Hellow, this is Borot'
+	app.run()
 
 @app.route('/heyborot', methods = ['POST'])
 def hey_borot():
 	return jsonify({'response' : 'Hi from Borot'})
 
 if __name__ == '__main__':
-	app.run()
+	main()
