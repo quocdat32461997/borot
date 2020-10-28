2fb04e59c0b35f2d35e336121fe4eaa4e6857370
Eric Ngo <quocdat32461997@yahoo.com>
2020-10-28 11:54:20 -0500

    Complete local-db connection initialization

--- lib/controllers/initialization.py
+++ lib/controllers/initialization.py
@@ -6,16 +6,36 @@ initialization.py - module for Database connections
 import os
 import pymysql
 
+from lib import configs
+
+def local_db():
+	"""
+	local_db - function to initialize connections to local MySQL
+	Inputs: None
+	Outputs: None
+	"""
+
+	connetion = pymysql.connect(host = configs.HOST, user = os.environ['LOCAL_DB_USER'], passwore = os.environ['LOCAL_DB_PASSWORD'], db = configs.DB)
+	return connection
+
+
+def aws_db():
+	"""
+	aws_db - function to initalize connections to AWS RDS
+	Inputs: None
+	Outputs: None
+	"""
+	return None
+
 def main(mode = 'local'):
 	# connect to AWS RDS
 	try:
 		# local db
 		if mode == 'local':
 			print("Connected to local MySQL")
+			return local_db()
 		# aws db
 		else:
 			print("Connected to AWS DS")
-	except Error as error:
+	except Exception as error:
 			print(error)
-			
-
