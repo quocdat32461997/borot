0f1b78a35be63f33a115870f2d2c0e7791926f54
Eric Ngo <quocdat32461997@yahoo.com>
2020-11-01 02:53:33 -0600

    Add path configs

--- lib/borot_ai/nlu.py
+++ lib/borot_ai/nlu.py
@@ -5,6 +5,10 @@ nlu.py - module for Natuarl Language Understanding tasks
 # import dependencies
 import os
 
+from .BiLSTM_CRF.bilstm_crf.inference import NameEntityRecognizer as NER
+from .intent_classifier.inference import IntentClassifier as IC
+from . import borotai_configs as configs
+
 def ner(ner, input):
 	"""
 	ner - fucntion for Name Entity Recognition
@@ -28,11 +32,11 @@ def ner(ner, input):
 
 	return tags
 
-def intent_detection(user, input):
+def intent_detection(ic, input):
 	"""
 	intent_detection - function for Intent Detection
 	Inputs:
-		- user: User class object
+		- ic: IntentClassifier object
 		- input: str
 			Raw query input
 	Outputs:
@@ -40,7 +44,9 @@ def intent_detection(user, input):
 			Detected intent
 	"""
 
-	return None
+	output = ic.predict(input)
+
+	return output
 
 def response_generator(user, input):
 	"""
@@ -56,3 +62,10 @@ def response_generator(user, input):
 
 	output = None
 	return output
+
+def initialize_borot_ai():
+	# initialize BiLSTM-CRF model
+	ner = NER(configs.BILSTM_CRF_MODEL, tag_table = configs.BILSTM_CRF_TAG_PATH, word_table = configs.BISLTM_CRF_WORD_PATH)
+
+
+	return ner
